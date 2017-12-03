6215cbab32ee6207cb898ec595292a5163211708
Andy <andy@unix.home>
2017-12-03 15:26:36 +0200

    changing objects state, button onClick

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -12,14 +12,30 @@ class App extends Component {
         ]
     };
 
+    switchNameHandler = () => {
+        console.log('Was clicked');
+        // Does not work this.state.persons[0].name = 'Andrew';
+        this.setState({
+            persons: [
+                {name: 'Timothy', age: 21},
+                {name: 'Dane', age: 23}
+            ]
+        })
+    };
+
+    switchNameHandler2 = () => {
+        console.log('no Mouse')
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Main header</h1>
                 <Person name="Bill" age="20"> My hobbies are traveling and reading.</Person>
-                <h4>{personAsVar()}</h4>
-                <h4>{personWithArgs({name:"Bill", sex:"male"})}</h4>
+                <h4 onClick={this.switchNameHandler}>{personAsVar()}</h4>
+                <h4>{personWithArgs({name: "Bill", sex: "male"})}</h4>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <button onClick={this.switchNameHandler} onMouseOut={this.switchNameHandler2}>Switch Button</button>
             </div>
         );
     }
